243cb49d450d68d8cf8e20b889abe90523df80d7
Lorenzo Ciglioni <lorenzo.ciglioni@gmail.com>
2025-01-02 15:12:23 +0100

    Completata traduzione interfaccia in italiano: - Aggiunto file traduzioni it.yml - Modificato link principale in 'Tutte le Timeline' - Gestito singolare/plurale per eventi - Mantenuto il termine Timeline in inglese

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,10 @@ Bundler.require(*Rails.groups)
 module TimelineApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults 7.0
+
+    # Set Italian as default locale
+    config.i18n.default_locale = :it
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers

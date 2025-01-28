72688f46f30534bc9943f66b6b777585eb9fd33c
Lorenzo Ciglioni <lorenzo.ciglioni@gmail.com>
2025-01-28 19:10:34 +0100

    Aggiunto calcolo settimane e mesi alle Timeline

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -1,3 +1,30 @@
 class Timeline < ApplicationRecord
 	has_many :events, dependent: :destroy
+
+	validates :name, presence: true
+
+	def duration_details
+		return nil if events.empty?
+		
+		# Inizializziamo i Set per settimane e mesi
+		weeks = Set.new
+		months = Set.new
+		
+		# Per ogni evento, aggiungiamo tutte le date dal suo inizio alla sua fine
+		events.each do |event|
+			end_date = event.end_date || Date.current
+			current_date = event.start_date
+			
+			while current_date <= end_date
+				weeks << current_date.beginning_of_week
+				months << current_date.beginning_of_month
+				current_date += 1.day
+			end
+		end
+
+		{
+			weeks: weeks.size,
+			months: months.size
+		}
+	end
 end
